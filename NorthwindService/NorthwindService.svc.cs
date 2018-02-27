e1d40f9b773959e9e016e3d9d1324e1cb0b3407d
Martin <martin-man2010@hotmail.co.uk>
2018-02-27 11:02:12 +0000

    Tuesday, 27 February 2018

--- NorthwindService/NorthwindService.svc.cs
+++ NorthwindService/NorthwindService.svc.cs
@@ -5,29 +5,21 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using NorthwindContext;
 
 namespace NorthwindService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class NorthwindService : INorthwindService
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
+        public List<string> GetNames(string cha)
+        {            
+            using (NorthwindEntities db = new NorthwindEntities())
+            {
+                return db.Customers.Select(x => x.CustomerID).Where(c => c.Contains(cha)).ToList();
+            }
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
     }
 }
