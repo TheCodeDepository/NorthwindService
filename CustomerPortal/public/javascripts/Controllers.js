372929f7a77697facb2294c06c5002f2224c45c7
Martin <martin-man2010@hotmail.co.uk>
2018-03-06 17:01:48 +0000

    Workv2

--- CustomerPortal/public/javascripts/Controllers.js
+++ CustomerPortal/public/javascripts/Controllers.js
@@ -8,19 +8,51 @@
         $scope.records = response.data;
     });
 
-    $scope.getCustomersByID = function (controlID) {
-        var id = document.getElementById(controlID).value;      
-        var customers = [];
-        for (var i = 0; i < $scope.dataPool.length; i++) {
-            var record = $scope.dataPool[i];
-            if (record.CustomerID === id) {
-                customers.push(record);
-            }
-        }    
-        $scope.records = customers;
-    };
-
+    //$scope.getCustomersByID = function (controlID) {
+    //    if (controlID === "") {
+    //        $scope.records = $scope.dataPool;
+    //    }
+    //    var id = document.getElementById(controlID).value;      
+    //    var customers = [];
+    //    for (var i = 0; i < $scope.dataPool.length; i++) {
+    //        var record = $scope.dataPool[i];
+    //        if (record.CustomerID === id) {
+    //            customers.push(record);
+    //        }
+    //    }    
+    //    $scope.records = customers;
+    //};
 });
+
+
+App.filter('idFilter', function () {
+    return function (input, searchParameter) {
+
+        var out = [];
+        if (searchParameter === "") {
+            return input;
+        }
+
+        else {
+
+            for (var i = 0; i < input.length; i++) {
+
+                var record = input[i].CustomerID.toString().toLowerCase();
+                var searchParam = searchParameter.toString().toLowerCase();
+
+                if (record.includes(searchParam)) {
+                    out.push(input[i]);
+                    console.log("found record");
+                }
+
+            }
+
+            return out;
+        }
+
+    };
+});
+
 
 App.controller('OrderCtrl', function ($scope, $http) {
 
